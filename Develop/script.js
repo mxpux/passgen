8406fd8a4b073bfee243fba163a0f221635ff647
MPx <hellomariap@gmail.com>
2021-06-22 20:40:38 -0700

    Added Var cases and special cases

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numChoice = [0,1,2,3,4,5,6,7,8,9];
+var upperCase = ["A","B","C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var spCase = ["~","!","@","#","$","%","^","&","*","(",")","-","_","+","=","{","}","|","[","]","<",">","?",",",".","/"];
 
 // Write password to the #password input
 function writePassword() {
